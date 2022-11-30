05d5fac708182d0688fca037e4c99171c6d1a915
Jade de Paulo <75194076+jadeplima@users.noreply.github.com>
2022-11-30 03:53:42 -0300

    Initial commit

---
+++ bacend/app/Http/Controllers/PerfilController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\PerfilRequest;
+use Illuminate\Http\Request;
+use App\Perfil;
+
+class PerfilController extends Controller
+{
+
+    private $perfil;
+
+    public function __construct(Perfil $perfil)
+    {
+        $this->perfil = $perfil;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $perfils = $this->perfil->paginate(10);
+
+        return view('admin.perfils.index', compact('perfils'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $perfils = $this->perfil->all('id','nome');
+        $action = route('admin.perfils.store');
+        $method = 'POST';
+
+        return view('admin.perfils.form', compact('perfils','action','method'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PerfilRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $data = $request->all();
+
+            $perfil = $this->perfil->create($data);
+
+            flash('Perfil Criado com sucesso!')->success();
+
+            DB::commit();
+
+            return redirect()->route('admin.perfils.index');
+        } catch (Exception $exeption) {
+            DB::rollBack();
+
+            return response()->json(['mensagem' => 'A solicitação não pode ser concluída, tente novamente mais tarde'],500);
+        };
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($perfil)
+    {
+        try {
+            DB::beginTransaction();
+
+            $perfil = $this->perfil->findOrFail($perfil);
+            $action = route('admin.perfils.update',['perfil' => $perfil->id]);
+            $method = 'PUT';
+
+            return view('admin.perfils.form', compact('perfil','action','method'));
+        }catch (Exception $exeption) {
+
+            DB::rollBack();
+
+            return response()->view('erros/404.blade.php');
+        };
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PerfilRequest $request, $perfil)
+    {
+        $data = $request->all();
+
+        $perfil = $this->perfil->findOrFail($perfil);
+        $perfil->update($data);
+
+        flash('Perfil Atualizado com sucesso!')->success();
+        return redirect()->route('admin.perfils.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $perfil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($perfil)
+    {
+
+        $perfil = $this->perfil->findOrFail($perfil);
+        $perfil->delete();
+
+        flash('Perfil Removido com sucesso!')->success();
+        return redirect()->route('admin.perfils.index');
+
+    }
+}
---
+++ bacend/app/Http/Controllers/UserController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserEditRequest;
+use Illuminate\Http\Request;
+use App\User;
+use App\Perfil;
+use TheSeer\Tokenizer\Exception;
+
+class UserController extends Controller
+{
+
+    private $user;
+    private $perfil;
+
+    public function __construct(User $user, Perfil $perfil)
+    {
+        $this->user = $user;
+        $this->perfil = $perfil;
+
+    }
+
+    public function index()
+    {
+        try {
+
+                $users = $this->user->paginate(5);
+
+                return view('admin.users.index', compact('users'));
+
+        } catch (Exception $exeption){
+             return response()->view('erros/500.blade.php');
+        };
+    }
+
+    public function create()
+    {
+        try {
+                $users = $this->user->all('id','name');
+                $action = route('admin.users.store');
+                $method = 'POST';
+                $perfis = $this->perfil->all('id','nome');
+
+                return view('admin.users.form', compact('users','perfis', 'action','method'));
+
+        }catch (Exception $exeption) {
+            return response()->view('erros/403.blade.php');
+        };
+    }
+
+    public function store(UserRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $data = $request->all();
+            $user = $this->user->create($data);
+
+            flash('Usuário Criado com sucesso!')->success();
+
+            DB::commit();
+            return redirect()->route('admin.users.index');
+        } catch (Exception $exeption) {
+            DB::rollBack();
+
+            return response()->json(['mensagem' => 'A solicitação não pode ser concluída, tente novamente mais tarde'],500);
+        };
+    }
+
+    public function edit($user)
+    {
+        try {
+            DB::beginTransaction();
+
+                $user = $this->user->findOrFail($user);
+
+                $action = route('admin.users.update',['user' => $user->id]);
+                $method = 'PUT';
+                $perfis = $this->perfil->all('id','nome');
+
+
+                DB::commit();
+
+                return view('admin.users.form', compact('user', 'perfis', 'action','method'));
+
+        }catch (Exception $exeption) {
+
+            DB::rollBack();
+
+            return response()->view('erros/404.blade.php');
+        };
+
+    }
+
+    public function update(UserEditRequest $request,  $user)
+    {
+        try {
+            $data = $request->all();
+
+            $user = $this->user->find($user);
+            if(!$request->filled('password')){
+                unset($data['password']);
+            }
+
+            $user->update($data);
+
+            flash('Dados Atualizados com sucesso!')->success();
+            return redirect()->route('admin.users.index');
+
+        } catch (Exception $exeption) {
+
+            return response()->json(['mensagem' => 'A solicitação não pode ser concluída, tente novamente mais tarde'],404);
+
+        };
+    }
+
+    public function destroy($user){
+        try {
+
+            $user = $this->user->find($user);
+            $user->delete();
+
+            flash('Usuário removido com sucesso!')->success();
+            return redirect()->route('admin.users.index');
+
+        } catch (Exception $exeption) {
+
+            return response()->json(['mensagem' => 'A solicitação não pode ser concluída, tente novamente mais tarde'],500);
+
+        };
+    }
+
+}
